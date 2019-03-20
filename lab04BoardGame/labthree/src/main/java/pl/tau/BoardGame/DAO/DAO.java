77787c30eb5db3af9809c21e62e76396d6281b80
AleksanderKozyra <olek980@wp.pl>
2019-03-20 23:10:01 +0100

    init lab04

---
+++ lab04BoardGame/labthree/src/main/java/pl/tau/BoardGame/DAO/DAO.java
@@ -0,0 +1,19 @@
+package pl.tau.BoardGame.DAO;
+
+import pl.tau.BoardGame.Domain.RPGfigure;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DAO<T> {
+
+    public Connection getConnection();
+    public void setConnection(Connection connection) throws SQLException;
+    public List<RPGfigure> getAllFigures();
+    public int addFigure(RPGfigure figure);
+    public int deleteFigure(RPGfigure figure) throws SQLException;
+    public int updateFigure(RPGfigure figure) throws SQLException;
+    public RPGfigure getFigure(long id) throws SQLException;
+
+}
---
+++ lab04BoardGame/labthree/src/main/java/pl/tau/BoardGame/Domain/RPGfigure.java
@@ -0,0 +1,70 @@
+package pl.tau.BoardGame.Domain;
+
+public class RPGfigure {
+
+    private Long id;
+    private String name;
+    private int HP;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public RPGfigure(Long id, String name, int HP) {
+        this.name = name;
+        this.id = null;
+        this.HP = HP;
+    }
+
+    public RPGfigure(){
+
+    }
+    @Override
+    public boolean equals(Object o) {
+        System.out.print(o.toString() + " rowna sie " + this);
+        if (this == o) {
+            System.out.println(true);
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            System.out.println(false);
+            return false;
+        }
+        RPGfigure rpGfigure = (RPGfigure) o;
+
+        boolean r = id.longValue() == rpGfigure.id.longValue() &&
+                HP == rpGfigure.HP &&
+                name.equals(rpGfigure.name);
+        System.out.print("ostatni warunek" + r);
+        return r;
+    }
+
+    @Override
+    public String toString() {
+        return "FigureDao{" +
+                "id=" + id +
+                ", name=" + name +
+                ", HP=" + HP +
+                '}';
+    }
+}
